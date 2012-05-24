52a1af044d39e3acca558f4fb234e4cc8d312c4c
Alberto Miranda <codealchemist@gmail.com>
2012-05-24 13:08:15 -0300

    quickly added first version of volume control; should detect range input compatibility and use this or select as fallback

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
     <!-- Le styles -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.20/themes/smoothness/jquery-ui.css" rel="stylesheet">
     <style>
       body {
         padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
@@ -52,14 +53,28 @@
 		<button id="playPause" class="btn btn-large btn-warning" onclick="Remora.api('togglePlayPause')">Play / Pause</button>
 		<button id="previous" class="btn btn-large btn-info" onclick="Remora.api('previous')"><i class="icon-fast-backward"></i> Previous</button>
 		<button id="next" class="btn btn-large btn-info" onclick="Remora.api('next')"><i class="icon-fast-forward"></i> Next</button>
+		<div style="margin:20px;">
+			<div id="volume" style="width:535px; float:left; margin:0 5px 0 0"></div>
+			<i class="icon-volume-up"></i>
+		</div>
     </div> <!-- /container -->
 
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/inject.js"></script>
     <script src="js/remora.local.js"></script>
+	<script>
+		$(document).ready(function(){
+			$('#volume').slider({
+				change: function(event, ui){
+					Remora.api('setVolume', ui.value);
+				}
+			});
+		});
+	</script>
   </body>
 </html>
